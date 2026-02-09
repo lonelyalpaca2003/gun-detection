1261ea00d324489cf9278cb9f15bace92791dc38
lonelyalpaca2003 <aravindrajeshmenon2003@gmail.com>
2026-02-09 12:49:53 +0000

    added a detection script

---
+++ scripts/detect.py
@@ -0,0 +1,17 @@
+from ultralytics import YOLO
+import cv2
+import sys
+
+def detect_gun(image_path, model_path = '../runs/detect/train/weights/best.pt'):
+    model = YOLO(model_path)
+    results = model.predict(source = image_path, conf = 0.5)
+    annotated = results[0].plot()
+
+    cv2.imwrite('result.jpg', annotated)
+
+    return results
+    
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print("WRong format, usage: python detect.py <image_path>")
