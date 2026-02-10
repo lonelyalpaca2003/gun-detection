66c98bc5fef749418dadda27df4227574d0076f4
lonelyalpaca2003 <aravindrajeshmenon2003@gmail.com>
2026-02-10 22:36:16 +0000

    slight change in function format

--- scripts/detect.py
+++ scripts/detect.py
@@ -2,7 +2,7 @@ from ultralytics import YOLO
 import cv2
 import sys
 
-def detect_gun(image_path, model_path = '../runs/detect/train/weights/best.pt'):
+def detect_gun(image_path, model_path = 'runs/detect/train/weights/best.pt'):
     model = YOLO(model_path)
     results = model.predict(source = image_path, conf = 0.5)
     annotated = results[0].plot()
@@ -15,3 +15,8 @@ def detect_gun(image_path, model_path = '../runs/detect/train/weights/best.pt'):
 if __name__ == "__main__":
     if len(sys.argv) < 2:
         print("WRong format, usage: python detect.py <image_path>")
+        sys.exit(1)
+    
+    image_path = sys.argv[1]
+    detect_gun(image_path)
+
